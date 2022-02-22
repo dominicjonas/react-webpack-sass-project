f97c3e1d88afd419741ebc25dbbd4c5786d3f1f4
Dominic Jonas <ppkk.dominic@gmail.com>
2022-02-22 12:29:57 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './components/Home'
+import './sass/style.css'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
